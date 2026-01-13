8c613d7bcb9182eafc367bcbdb77b7c53267ef70
awasthinayan <nayanawasthi2015@gmail.com>
2026-01-13 11:46:24 +0530

    server check

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-import express from "express";
-import { PORT } from "./src/Config/serverConfig.js";
-import connectDB from "./src/Config/DBConfig.js";
-import Routes from "./src/Routes/UserRoute.js";
-
+import express from 'express';
+import { PORT } from './src/Config/serverConfig.js';
+import connectDB from './src/Config/DBConfig.js';
+import Routes from './src/Routes/UserRoute.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 connectDB();
 
-app.use("/V1/api",Routes)
+app.use('/V1/api', Routes);
 
-app.get("/start", (req, res) => {
-  res.send("Hello World the server is running");
+app.get('/', (req, res) => {
+  res.send('Hello World the server is running');
 });
 
 app.listen(PORT, () => {
